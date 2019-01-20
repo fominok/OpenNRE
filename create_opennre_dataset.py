a266b9273c8fa8b41925dd740254f8b5e6adea35
Evgeny Fomin <fominok@hotmail.com>
2019-01-20 15:42:15 +0300

    add comments to semeval-related scripts

--- create_opennre_dataset.py
+++ create_opennre_dataset.py
@@ -1,20 +1,10 @@
-
-"""
-The original KBP37 dataset has a structure like in those 2 examples:
-    15917   " The <e1> Mississippi </e1> Air National Guard ( <e2> MS ANG </e2> ) is the air force militia of the State of Mississippi United States of America . "
-    org:alternate_names(e2,e1)
-
-
-    15918   " When the Gauche was excluded from the SFIO he became one of the leaders of the new Parti Socialiste Ouvrier et Paysan ( <e1> PSOP </e1> - Workers and Peasants <e2> Socialist Party </e2> ) and was at that time quite close to Leon Trotsky . "
-    org:alternate_names(e2,e1)
-
---> we want to bring it into the JSON file format of OpenNRE!
-
-"""
-
-
-import json, sys, re, hashlib
+import json
+import sys
+import re
+import hashlib
 from pprint import pprint
+
+# Reading SemEval dataset contents
 
 f = open(sys.argv[1]).readlines()
 
@@ -27,102 +17,96 @@ for line in f:
     else:
         buf.append(line.strip())
 
+f.close()
+
 print(len(blocks))
 
 examples = []
 all_rels = set()
 
+# Each block represents a dataset record
 for block in blocks:
+    # rel_index stands for a number of line where relation info located in
+    # Usually it is a second line but it's not guaranteed
     rel_index = 0
     rel_line = None
     for i, line in enumerate(block):
         if line.find('(e1,e2)') > 0 or line.find('(e2,e1)') > 0 or line == 'Other':
-            # Save relation index; data above is sentence, below -- comments
             rel_index = i
             rel_line = line
             break
 
+    # If no line in a record contains relation info (any relation or Other)
+    # dataset is probably broken
     assert(rel_index != 0), 'No relation data for block {}'.format(block)
 
-    main = re.search(r'"(.+)"$', ''.join(block[:rel_index]))[1] # Getting the sentence
+    # Getting the sentence
+    # We use block[:rel_index] here because sentense is all text before relation line
+    main = re.search(r'"(.+)"$', ''.join(block[:rel_index]))[1]
 
+    # Find words with relation information provided
     e1 = re.search('<e1>(.*)</e1>', main).group(1).strip()
     e2 = re.search('<e2>(.*)</e2>', main).group(1).strip()
-    cl = main.replace('<e1>', '').replace('<e2>', '').replace('</e1>', '').replace('</e2>', '')
+
+    # Cleanup sentence from tags as OpenNRE dataset uses clean sentences without tag embeddings
+    cl = main.replace('<e1>', '').replace(
+        '<e2>', '').replace('</e1>', '').replace('</e2>', '')
     print(cl, e1, e2)
 
+    # Relation type is written right before parens
     if rel_line.find('e1,e2') > 0:
         rel_type = rel_line.split('(')[0]
         print('rel_type', rel_type)
-
+    # Related words may be placed in reverse order
     elif rel_line.find('e2,e1') > 0:
         rel_type = rel_line.split('(')[0]
+        # So we have to swap them
         e1, e2 = e2, e1
-
     elif rel_line == 'Other':
         rel_type = 'NA'
-
     else:
         raise Exception('Cannot parse relation: {}'.format(block))
 
-    # else line == 'no_relation':
-    #     rel_type = 'no_relation' 
-
-    #elif len(line.strip()) == 0:
-        ## add new example
-        
-    if e1:
-        all_rels.add(rel_type)
-        example = {'e1': e1, 'e2': e2, 'rel_type': rel_type, 'sentence': cl} 
-        examples.append(example)
-        e1 = ''
-
-    else:
-        raise Exception('What is this line: ***', block, '***')
+    assert(e1 and e2 and rel_type), 'Error parsing relation data: {}'.format(block)
+    all_rels.add(rel_type)
+    example = {'e1': e1, 'e2': e2, 'rel_type': rel_type, 'sentence': cl}
+    examples.append(example)
 
 print("Number of examples", len(examples))
 
 
-def create_OpenNRE_dict(data):
+def create_open_nre_dict(data):
+    res = []
+    for item in data:
+        # Every words requires its own id, same words have same ids
+        idh = hashlib.md5(item['e1'].encode('utf8')).hexdigest()
+        idt = hashlib.md5(item['e2'].encode('utf8')).hexdigest()
 
-        res = []
-
-        for item in data:
-
-            ## convert all relation types (except the NEGATIVE_TYPE) to BINARY_TYPE
-            ## so for example "headquarter" --> 'locatedIn' .. 
-
-            idh = hashlib.md5(item['e1'].encode('utf8')).hexdigest()
-            idt = hashlib.md5(item['e2'].encode('utf8')).hexdigest()
-
-            new_entry = { 'head': {'type':'UNK',  'word':item['e1'],  'id': idh},
-                          'tail': {'type':'UNK',  'word':item['e2'],  'id': idt},
-                          'relation': item['rel_type'],
-                          'sentence': item['sentence']}
-
-            print(new_entry)
-            res.append(new_entry)
-
-        print('Number of items in dataset:', len(res))
-        return res
+        # Create a dataset record in a proper OpenNRE format
+        new_entry = {'head': {'type': 'UNK',  'word': item['e1'],  'id': idh},
+                     'tail': {'type': 'UNK',  'word': item['e2'],  'id': idt},
+                     'relation': item['rel_type'],
+                     'sentence': item['sentence']}
+        print(new_entry)
+        res.append(new_entry)
+    return res
 
 
-json_data = create_OpenNRE_dict(examples)
+json_data = create_open_nre_dict(examples)
 print(all_rels)
 print(len(all_rels))
 
-## create rel2id
+# create rel2id
 rel2id = {'NA': 0}
-all_rels_set = set(all_rels)
-all_rels_set.remove('NA')
+all_rels.remove('NA')
 i = 1
-for rel_type in all_rels_set:
+for rel_type in all_rels:
     rel2id[rel_type] = i
     i = i + 1
 
 print(rel2id)
 
-
+# Write dataset into json file
 with open('train.json', 'w') as outfile:
     json.dump(json_data, outfile)
 
@@ -130,5 +114,3 @@ with open('rel2id.json', 'w') as outfile:
     json.dump(rel2id, outfile)
 
 print(len(examples))
-
-
--- nre_to_scorer.py
+++ nre_to_scorer.py
@@ -1,6 +1,8 @@
 import json
 import sys
 
+# Load OpenNRE dataset with test results
+
 if len(sys.argv) > 1:
     dataset_name = sys.argv[1]
 
@@ -18,9 +20,14 @@ with open(f'data/{dataset_name}/test.json') as f:
 answers = {}
 for i, t in enumerate(test_data):
     sent = t['sentence']
+    # Find positions of words in dataset record
     e1_index = sent.find(t['head']['word'])
     e2_index = sent.find(t['tail']['word'])
+    # Ensure that both related words are found
     assert e1_index is not None and e2_index is not None
+
+    # For SemEval order of related words is stored in (e2,e1) or (e1,e2) format
+    # Using indexes of words we can restore this information
     order_direct = e1_index < e2_index
     entpair_1 = t['head']['id']
     entpair_2 = t['tail']['id']
@@ -28,6 +35,7 @@ for i, t in enumerate(test_data):
         'order_direct': order_direct,
         'relation_text': t['relation'],
         'relation_id': rels[t['relation']],
+        # Entpair is a concat of word ids from dataset used in test results
         'entpair': entpair_1 + '#' + entpair_2,
         'index': i + 1,
     }
@@ -43,6 +51,7 @@ for tr in test_results:
         if tr_old['score'] < tr['score']:
             test_results_best[e] = tr
 
+# Convert answers from OpenNRE test data format to SemEval scorer
 with open('answer_keys.txt', 'w') as f:
     for _, a in answers.items():
         rel_text = a['relation_text']
@@ -52,10 +61,12 @@ with open('answer_keys.txt', 'w') as f:
             rel = rel_text + ('(e1,e2)' if a['order_direct'] else '(e2,e1)')
         f.write(str(a['index']) + '\t' + rel + '\n')
 
+# Convert proposed answers from OpenNRE format with entpairs to SemEval scorer
 with open('proposed_anwsers.txt', 'w') as f:
     for entpair, p in test_results_best.items():
         a = answers[entpair]
         order_direct = a['order_direct']
-        rel = rels_rev[p['relation']] + ('(e1,e2)' if order_direct else '(e2,e1)')
+        rel = rels_rev[p['relation']] + \
+            ('(e1,e2)' if order_direct else '(e2,e1)')
         index = a['index']
         f.write(str(index) + '\t' + rel + '\n')
